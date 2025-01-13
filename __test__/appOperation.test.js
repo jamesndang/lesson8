360526c0fefd3995a5f482f2752457c378958639
James Dang <james.dang@docusign.com>
2025-01-13 11:31:36 -0800

    added device feature

--- __test__/appOperation.test.js
+++ __test__/appOperation.test.js
@@ -1,5 +1,17 @@
 const appOperations = require('../src/appOperations');
 
-TextDecoderStream("multiplication property of zero", ()=> {
-    expected(appOperations(5,0)).toBe(0);
+test("multiplication property of zero", ()=> {
+    expect(appOperations.multiply(5,0)).toBe(0);
+});
+
+test("adding two values", ()=> {
+    expect(appOperations.add(5,5)).toBe(10);
+});
+
+test("test subtraction", ()=> {
+    expect(appOperations.subtract(15,5)).toBe(10);
+});
+
+test("test devide", ()=> {
+    expect(appOperations.devide(15,5)).toBe(3);
 });
--- src/appOperation.js
+++ src/appOperation.js
@@ -2,3 +2,18 @@ function multiply(num1,num2) {
     return num1*num2;
 }
 module.exports = multiply;
+
+function subtract(num1, num2) {
+    return num1-num2;
+}
+
+function devide(num1, num2) {
+    return num1/num2;
+}
+
+module.exports = {
+    multiply,
+    add,
+    subtract,
+    devide
+}
